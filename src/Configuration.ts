aa30147231839c8bda5ccff77e4ffc6226ae8191
lautarodragan <lautaro.dragan@gmail.com>
2017-11-04 19:26:19 -0300

    add config options

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -2,7 +2,12 @@ export interface Configuration {
   readonly rabbitmqUrl: string
   readonly mongodbUrl: string
   readonly ipfsUrl: string
+  readonly insightUrl: string
   readonly s3Url: string
 
+  readonly poetNetwork: string
+  readonly poetVersion: ReadonlyArray<number>
   readonly minimumBlockHeight: number
+  readonly bitcoinAddress: string
+  readonly bitcoinAddressPrivateKey: string
 }
