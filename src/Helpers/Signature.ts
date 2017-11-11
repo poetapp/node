32b71a77d4cd84b82de780afb03649359381d49d
lautarodragan <lautaro.dragan@gmail.com>
2017-11-11 17:37:07 -0300

    Add empty .id field validation to SignatureHelper.signClaim

--- src/Helpers/Signature.ts
+++ src/Helpers/Signature.ts
@@ -1,9 +1,12 @@
 import * as bitcore from 'bitcore-lib'
 
+import { IllegalArgumentException } from '../API/Exceptions'
 import { Claim } from '../Interfaces'
 
 export namespace Signature {
   export function signClaim(claim: Claim, privateKey: string): string {
+    if (!claim.id)
+      throw new IllegalArgumentException('Cannot sign a claim that has an empty .id field.')
     const signature = bitcore.crypto.ECDSA.sign(Buffer.from(claim.id, 'hex'), bitcore.PrivateKey(privateKey))
     return signature.toString()
   }
--- test/Signature.ts
+++ test/Signature.ts
@@ -3,6 +3,7 @@ import { Expect, Test, TestCase } from 'alsatian'
 import { Signature } from '../src/Helpers/Signature'
 import { Work } from '../src/Interfaces'
 import { PrivateKey, TheRaven } from './Claims'
+import {IllegalArgumentException} from '../src/API/Exceptions'
 
 export class SignatureTest {
 
@@ -11,5 +12,12 @@ export class SignatureTest {
   public signature(work: Work) {
     const signature = Signature.signClaim(work, PrivateKey)
     Expect(signature.toString()).toBe(work.signature)
+  }
+
+  @Test()
+  @TestCase(TheRaven)
+  public signatureWithoutId(work: Work) {
+    Expect(() => Signature.signClaim({...work, id: ''}, PrivateKey))
+      .toThrowError(IllegalArgumentException, 'Cannot sign a claim that has an empty .id field.')
   }
 }
