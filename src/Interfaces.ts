8dbdd2187b228dc105d8b987cc33d801ab0ad449
lautarodragan <lautaro.dragan@gmail.com>
2017-11-11 02:48:18 -0300

    Improve Claim interface, add Work interface

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -1,9 +1,35 @@
-export interface Claim {
-  readonly id: string
-  readonly publicKey: string
-  readonly signature: string
-  readonly type: string
-  readonly attributes: {
-    readonly [index: string]: string
-  }
+/**
+ * General interfaces for types used by the Po.et Node.
+ * These will be moved to poet-js in the future.
+ */
+
+export interface Claim<T extends ClaimAttributes = ClaimAttributes> {
+  readonly id?: string
+
+  readonly publicKey?: string
+  readonly signature?: string
+  readonly dateCreated?: Date
+
+  readonly type: ClaimType
+  readonly attributes: T
+}
+
+export interface ClaimAttributes {
+  readonly [key: string]: string
+}
+
+export enum ClaimType {
+  Work = 'Work'
+}
+
+export interface Work extends Claim<WorkAttributes> {}
+
+export interface WorkAttributes extends ClaimAttributes {
+  readonly name: string
+  readonly datePublished: string
+  readonly dateCreated: string
+  readonly author: string
+  readonly lastModified: string
+  readonly tags: string
+  readonly content: string
 }
