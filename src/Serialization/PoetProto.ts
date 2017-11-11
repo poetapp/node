b8561ae73051e3c0162c3e4b3cb560c715b5cd37
lautarodragan <lautaro.dragan@gmail.com>
2017-11-11 02:44:21 -0300

    Add Po.et Protobuf

---
+++ src/Serialization/PoetProto.ts
@@ -0,0 +1,8 @@
+import { Root, Type, INamespace} from 'protobufjs'
+
+import * as PoetProto from './PoetProto.json'
+
+const poetProtoRoot = Root.fromJSON(PoetProto as INamespace)
+
+export const ClaimProto = poetProtoRoot.lookup('Poet.Claim') as Type
+export const AttributeProto = poetProtoRoot.lookup('Poet.Attribute') as Type
