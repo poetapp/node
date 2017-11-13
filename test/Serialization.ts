d6e00d4e63945e226bab13b359080f6bb164f1f5
lautarodragan <lautaro.dragan@gmail.com>
2017-11-13 11:40:49 -0300

    Add unit tests for Serialization.claimToHex

--- test/Serialization.ts
+++ test/Serialization.ts
@@ -1,8 +1,22 @@
 import { Expect, Test, TestCase } from 'alsatian'
 
 import { Serialization } from '../src/Helpers/Serialization'
-import { Work } from '../src/Interfaces'
-import { TheRaven } from './Claims'
+import { Claim, ClaimAttributes, ClaimType, Work } from '../src/Interfaces'
+import { TheRaven, TheRavenHex } from './Claims'
+
+/**
+ * Simple function to help editing a Claim's attributes in an immutable fashion.
+ * TODO: support generics once https://github.com/Microsoft/TypeScript/issues/10727 is fixed.
+ */
+function editAttributes(claim: Claim, attributes: ClaimAttributes): Claim {
+  return {
+    ...claim,
+    attributes: {
+      ...claim.attributes,
+      ...attributes
+    }
+  }
+}
 
 export class SerializationTest {
 
@@ -33,5 +47,51 @@ export class SerializationTest {
       signature: '123'
     })
     Expect(ignoreSignature).toBe(work.id)
+  }
+
+  @Test()
+  @TestCase(TheRaven)
+  public claimToHex(work: Work) {
+    const serializedClaim = Serialization.claimToHex(work)
+    Expect(serializedClaim).toBe(TheRavenHex)
+  }
+
+  @Test()
+  @TestCase(TheRaven)
+  public claimToHexFailIfChangedAttribute(work: Work) {
+    // Changing any field should change the serialized too
+
+    Expect(Serialization.claimToHex(editAttributes(work, {
+      name: 'Nevermore'
+    }))).not.toBe(TheRavenHex)
+
+    Expect(Serialization.claimToHex(editAttributes(work, {
+      author: 'E.A.P.'
+    }))).not.toBe(TheRavenHex)
+
+    Expect(Serialization.claimToHex(editAttributes(work, {
+      dateCreated: (new Date()).toISOString()
+    }))).not.toBe(TheRavenHex)
+  }
+
+  @Test()
+  @TestCase(TheRaven)
+  public claimToHexFailIfChangedClaimData(work: Work) {
+    // Changing any field should change the serialized too
+
+    Expect(Serialization.claimToHex({
+      ...work,
+      publicKey: 'a' + work.publicKey.slice(1)
+    })).not.toBe(TheRavenHex)
+
+    Expect(Serialization.claimToHex({
+      ...work,
+      signature: 'b' + work.signature.slice(1)
+    })).not.toBe(TheRavenHex)
+
+    Expect(Serialization.claimToHex({
+      ...work,
+      type: 'Asd' as ClaimType
+    })).not.toBe(TheRavenHex)
   }
 }
