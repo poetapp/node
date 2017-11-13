4c174028095ae43b45ab5dd47371877b2148dd1c
lautarodragan <lautaro.dragan@gmail.com>
2017-11-13 14:45:43 -0300

    Add more validations to SignatureHelper.signClaim

--- src/Helpers/Signature.ts
+++ src/Helpers/Signature.ts
@@ -2,11 +2,19 @@ import * as bitcore from 'bitcore-lib'
 
 import { IllegalArgumentException } from '../API/Exceptions'
 import { Claim } from '../Interfaces'
+import { Serialization } from './Serialization'
 
 export namespace Signature {
   export function signClaim(claim: Claim, privateKey: string): string {
+    if (!claim.publicKey)
+      throw new IllegalArgumentException('Cannot sign a claim that has an empty .publicKey field.')
+    if (new bitcore.PrivateKey(privateKey).publicKey.toString() !== claim.publicKey)
+      throw new IllegalArgumentException('Cannot sign this claim with the provided privateKey. It doesn\t match the claim\'s public key.')
     if (!claim.id)
       throw new IllegalArgumentException('Cannot sign a claim that has an empty .id field.')
+    if (claim.id !== Serialization.getClaimId(claim))
+      throw new IllegalArgumentException('Cannot sign a claim whose id has been altered or generated incorrectly.')
+
     const signature = bitcore.crypto.ECDSA.sign(Buffer.from(claim.id, 'hex'), bitcore.PrivateKey(privateKey))
     return signature.toString()
   }
