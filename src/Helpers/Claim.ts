5192c437d7297d18a275c4bdbbf07ad2f989e821
lautarodragan <lautaro.dragan@gmail.com>
2017-11-17 20:19:00 -0300

    move signClaim to Helpers\Claim

--- src/Helpers/Claim.ts
+++ src/Helpers/Claim.ts
@@ -1,12 +1,31 @@
 import * as bitcore from 'bitcore-lib'
 
-import { Claim, ClaimAttributes, ClaimType, Work } from '../Interfaces'
+import { IllegalArgumentException } from 'API/Exceptions'
+import { Claim, ClaimAttributes, ClaimType, Work } from 'Interfaces'
+
 import { Serialization } from './Serialization'
-import { Signature } from './Signature'
 
 export function isClaim(object: any): object is Claim {
   // TODO: use joi or protobuf
   return object.id && object.publicKey && object.signature && object.type && object.attributes
+}
+
+export function isWork(claim: Claim): claim is Work {
+  return claim.type === ClaimType.Work
+}
+
+export function signClaim(claim: Claim, privateKey: string): string {
+  if (!claim.publicKey)
+    throw new IllegalArgumentException('Cannot sign a claim that has an empty .publicKey field.')
+  if (new bitcore.PrivateKey(privateKey).publicKey.toString() !== claim.publicKey)
+    throw new IllegalArgumentException('Cannot sign this claim with the provided privateKey. It doesn\t match the claim\'s public key.')
+  if (!claim.id)
+    throw new IllegalArgumentException('Cannot sign a claim that has an empty .id field.')
+  if (claim.id !== Serialization.getClaimId(claim))
+    throw new IllegalArgumentException('Cannot sign a claim whose id has been altered or generated incorrectly.')
+
+  const signature = bitcore.crypto.ECDSA.sign(Buffer.from(claim.id, 'hex'), bitcore.PrivateKey(privateKey))
+  return signature.toString()
 }
 
 export function isValidSignature(claim: Claim): boolean {
@@ -15,10 +34,6 @@ export function isValidSignature(claim: Claim): boolean {
     bitcore.crypto.Signature.fromString(claim.signature),
     new bitcore.PublicKey(claim.publicKey)
   )
-}
-
-export function isWork(claim: Claim): claim is Work {
-  return claim.type === ClaimType.Work
 }
 
 export function createClaim(privateKey: string, type: ClaimType, attributes: ClaimAttributes): Claim {
@@ -31,7 +46,7 @@ export function createClaim(privateKey: string, type: ClaimType, attributes: Cla
     attributes
   }
   const id = Serialization.getClaimId(claim)
-  const signature = Signature.signClaim({
+  const signature = signClaim({
     ...claim,
     id
   }, privateKey)
