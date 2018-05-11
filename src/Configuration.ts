3845690a00f76e26be76290124257adb851407f5
Lautaro Dragan <lautaro.dragan@gmail.com>
2018-05-11 16:13:27 -0300

    fixes (#102)
    * conf env
    
    * functional
     I will merge and test out in my branch

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -32,10 +32,10 @@ export interface LoggingConfiguration {
 }
 
 const defaultConfiguration: Configuration = {
-  rabbitmqUrl: process.env.RABBITMQ_URL || 'amqp://localhost',
-  mongodbUrl: process.env.MONGODB_URL || 'mongodb://localhost:27017/poet',
-  ipfsUrl: process.env.IPFS_URL || 'http://localhost:5001',
-  insightUrl: process.env.INSIGHT_URL || 'https://test-insight.bitpay.com/api',
+  rabbitmqUrl: 'amqp://localhost',
+  mongodbUrl: 'mongodb://localhost:27017/poet',
+  ipfsUrl: 'http://localhost:5001',
+  insightUrl: 'https://test-insight.bitpay.com/api',
 
   apiPort: 18080,
   poetNetwork: 'BARD',
@@ -55,10 +55,14 @@ const defaultConfiguration: Configuration = {
 export const configurationPath = () => path.join(homedir(), '/.po.et/configuration.json')
 
 export function loadConfigurationWithDefaults(): Configuration {
-  return { ...defaultConfiguration, ...loadConfiguration(configurationPath()) }
+  return {
+    ...defaultConfiguration,
+    ...loadConfigurationFromEnv(),
+    ...loadConfigurationFromFile(configurationPath()),
+  }
 }
 
-function loadConfiguration(configPath: string): Configuration | {} {
+function loadConfigurationFromFile(configPath: string): Configuration | {} {
   if (!existsSync(configPath)) {
     console.log('File', configPath, 'not found')
     return {}
@@ -73,6 +77,22 @@ function loadConfiguration(configPath: string): Configuration | {} {
   if (typeof configuration.poetVersion === 'object') validatePoetVersion(configuration.poetVersion)
 
   return configuration
+}
+
+function loadConfigurationFromEnv(): Partial<Configuration> {
+  // TODO: programmatically generate keys from values by applying camelCase to SCREAMING_SNAKE_CASE
+  const map: { [index: string]: string } = {
+    RABBITMQ_URL: 'rabbitmqUrl',
+    MONGODB_URL: 'mongodbUrl',
+    IPFS_URL: 'ipfsUrl',
+    INSIGHT_URL: 'insightUrl',
+  }
+
+  const configurationFromEnv = Object.entries(process.env)
+    .filter(([key, value]) => map[key])
+    .reduce((previousValue, [key, value]) => ({ ...previousValue, [map[key]]: value }), {})
+
+  return configurationFromEnv
 }
 
 function validatePoetVersion(poetVersion: any) {
