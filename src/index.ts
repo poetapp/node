d4f5559520a04fae3c31feadd12b18c29438b1d7
warren vosper <straydogsw@gmail.com>
2018-10-19 09:24:10 -0600

    refactor: remove console.log (#591)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-/* tslint:disable:no-console */
 import { app } from 'app'
+import * as Pino from 'pino'
+
+const logger: Pino.Logger = Pino()
 
 app()
   .then(server => process.on('SIGINT', () => server.stop()))
-  .catch(console.error)
+  .catch(exception => logger.error({ exception }, 'server was unable to start'))
