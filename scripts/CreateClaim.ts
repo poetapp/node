2b361ca725a81adabe5878d87c020dc916d499f6
Lautaro Dragan <lautaro.dragan@gmail.com>
2018-10-19 03:34:25 -0300

    chore(scripts): clean output of npm run create-claim (#595)

--- scripts/CreateClaim.ts
+++ scripts/CreateClaim.ts
@@ -5,7 +5,6 @@ import { PrivateKeyACD } from '../test/Claims'
 import { Client } from '../test/Integration/Helper'
 
 const main = async () => {
-  console.log(process.argv[1], process.argv[2], process.argv[3], process.argv[4])
   const attributes = {
     name: process.argv[2] || 'testing claim',
     author: process.argv[3] || 'the tester',
@@ -16,8 +15,6 @@ const main = async () => {
   console.log(JSON.stringify(claim, null, 2))
 
   const response = await client.postWork(claim)
-
-  console.log(JSON.stringify(await response.text(), null, 2))
 }
 
 main().catch(console.error)
