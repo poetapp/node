96eb4658332e9d221818e4f633d39b74389d53ce
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-11-22 18:20:42 -0300

    test: Improve Stress Code Create Work (#803)

--- tests/stress/createWorks.js
+++ tests/stress/createWorks.js
@@ -3,6 +3,7 @@ import { check, sleep } from 'k6'
 
 const CLAIM_HOST = __ENV.CLAIM_HOST || 'http://0.0.0.0:3002' 
 const NODE_HOST = __ENV.NODE_HOST || 'http://0.0.0.0:18080'
+const DELAY_GET_WORK =  __ENV.DELAY_GET_WORK || 0
 
 export default () => {
 
@@ -11,11 +12,21 @@ export default () => {
   const url = `${NODE_HOST}/works`
   const payload = requestClaim.body
   const params =  { headers: { 'Content-Type': 'application/json' } }
-  const res = http.post(url, payload, params)
+  const work = http.post(url, payload, params)
+
+  check(work, {
+    'status 202': (r) => r.status === 202
+  })
+
+  sleep(1 + parseInt(DELAY_GET_WORK))
+
+  const id = JSON.parse(work.request.body).id
+  const urlGetWork = `${NODE_HOST}/works/${id}`
+  const res = http.get(urlGetWork)
 
   check(res, {
-    'status 202': (r) => r.status === 202
-  });
+    'status 200': (r) => r.status === 200
+  })
 
   sleep(1)
 }
