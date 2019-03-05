bfe51accf7b4537afa832cace4700fcba6de120b
WesleyCharlesBlake <wesley@po.et>
2019-03-05 14:41:43 +0200

    revert back to node image

--- Docker/tools/wait-for-it.sh
+++ Docker/tools/wait-for-it.sh
@@ -1,79 +1,30 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-TIMEOUT=15
-QUIET=0
-
-echoerr() {
-  if [ "$QUIET" -ne 1 ]; then printf "%s\n" "$*" 1>&2; fi
-}
-
-usage() {
-  exitcode="$1"
-  cat << USAGE >&2
-Usage:
-  $cmdname host:port [-t timeout] [-- command args]
-  -q | --quiet                        Do not output any status messages
-  -t TIMEOUT | --timeout=timeout      Timeout in seconds, zero for no timeout
-  -- COMMAND ARGS                     Execute command with args after the test finishes
-USAGE
-  exit "$exitcode"
-}
-
-wait_for() {
-  for i in `seq $TIMEOUT` ; do
-    nc -z "$HOST" "$PORT" > /dev/null 2>&1
-    
-    result=$?
-    if [ $result -eq 0 ] ; then
-      if [ $# -gt 0 ] ; then
-        exec "$@"
-      fi
-      exit 0
-    fi
-    sleep 1
-  done
-  echo "Operation timed out" >&2
-  exit 1
-}
-
-while [ $# -gt 0 ]
-do
-  case "$1" in
-    *:* )
-    HOST=$(printf "%s\n" "$1"| cut -d : -f 1)
-    PORT=$(printf "%s\n" "$1"| cut -d : -f 2)
-    shift 1
-    ;;
-    -q | --quiet)
-    QUIET=1
-    shift 1
-    ;;
-    -t)
-    TIMEOUT="$2"
-    if [ "$TIMEOUT" = "" ]; then break; fi
-    shift 2
-    ;;
-    --timeout=*)
-    TIMEOUT="${1#*=}"
-    shift 1
-    ;;
-    --)
-    shift
-    break
-    ;;
-    --help)
-    usage 0
-    ;;
-    *)
-    echoerr "Unknown argument: $1"
-    usage 1
-    ;;
-  esac
-done
-
-if [ "$HOST" = "" -o "$PORT" = "" ]; then
-  echoerr "Error: you need to provide a host and port to test."
-  usage 2
+if [ "$#" -ne 1 ]; then
+    >&2 echo "Usage: $0 host:port"
+    exit -1
 fi
 
-wait_for "$@"
+ARGUMENT=$1
+HOST="$(echo $ARGUMENT | cut -d ':' -f1)"
+PORT="$(echo $ARGUMENT | cut -d ':' -f2)"
+MAX_RETRY=90
+
+echo "Testing connection to host $HOST and port $PORT."
+
+count=0
+while [ $count -lt $MAX_RETRY ]
+do
+    count=$((count+1))
+    nc -z $HOST $PORT
+    result=$?
+    if [ $result -eq 0 ]; then
+        echo "Connection is available after $count second(s)."
+        exit 0
+    fi
+    echo "Retrying..."
+    sleep 1
+done
+
+>&2 echo "Timeout occurred after waiting $MAX_RETRY seconds for $HOST:$PORT."
+exit -1
